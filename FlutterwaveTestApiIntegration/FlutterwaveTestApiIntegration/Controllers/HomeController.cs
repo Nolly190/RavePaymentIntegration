26d5aab3c6a8a8f775c3828ac75da1dbd82804ca
udenwani nonso <nolly190@gmail.com>
2018-06-12 20:40:57 +0100

    The Main Project

---
+++ FlutterwaveTestApiIntegration/FlutterwaveTestApiIntegration/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using FlutterwaveTestApiIntegration.App_Code;
+
+
+
+namespace FlutterwaveTestApiIntegration.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+           
+            return View();
+        }
+
+
+
+
+      
+
+
+
+    public ActionResult Validate(string UserId)
+        {
+            
+        var data = new { txref = "rave-123568", SECKEY = "FLWSECK-7766fb06695e63a44f9145fedc89bcb6-X", include_payment_entity = 1 };
+        var client = new HttpClient(); /*OH - AAED44*/
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var responseMessage = client.PostAsJsonAsync("https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/xrequery", data).Result;
+        var responseStr = responseMessage.Content.ReadAsStringAsync().Result;
+        var response = Newtonsoft.Json.JsonConvert.DeserializeObject<ApiResponse>(responseStr);
+            if (response.data.status == "successful" && response.data.amount == "2000" && response.data.chargecode == "00")
+            {
+
+                ViewBag.Status = "Successful";
+
+            }
+            else
+            {
+                ViewBag.Status = "Failed";
+            }
+            return View();
+        }
+    }
+}
---
+++ FlutterwaveTestApiIntegration/FlutterwaveTestApiIntegration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FlutterwaveTestApiIntegration.MvcApplication" Language="C#" %>
